d7ee3e7a36eda8c8429312b20b1b8793784713fa
Sam Steingold <sds@gnu.org>
2015-03-25 16:58:17 -0400

    project to categories

--- wikigraph.scala
+++ wikigraph.scala
@@ -23,22 +23,41 @@ val vertexes = sc.textFile(home+"vertex_ids.txt").map(s => { // 4831649
   (v(0),v(1).toLong)}).cache() // (page name, page ID)
 
 // use fullOuterJoin to also get vertixes without ID
-val page_MD = categories.rightOuterJoin(vertexes).map(_ match {
+val page_MD = categories.rightOuterJoin(vertexes).map({ // 4,831,649
   case (pagen,(Some(cats),id)) => (id,(pagen,cats))
-  case (pagen,(None,id)) => (id,(pagen,Array()))
+  case (pagen,(None,id)) => (id,(pagen,Array[(String,Double)]()))
 }).cache()
 
-val adjacency = edges.flatMap(s => { // 116330611
+val adjacency = edges.flatMap(s => { // 116,330,611
   val v = s.split("\t")
   val beg = v.head.toLong
-  v.tail.groupBy(identity).map(t => (t._1, t._2.length)).toSeq.map(_ match {
+  v.tail.groupBy(identity).map(t => (t._1, t._2.length)).toSeq.map({
     case (v,w) => Edge(beg,v.toLong,w)
   })
 }).cache()
 
-
-
 val wg = Graph.apply(page_MD,adjacency).cache()
 
-// category ==> total weight
+// category ==> total weight (363 rows)
 val category_table = categories.flatMap(t => t._2).reduceByKey(_+_).collect()
+
+// project to categories (125784 rows)
+val category_edges = adjacency.map({
+  case Edge (src, dst, weight) => (src, (dst, weight))
+}).join(page_MD).map({
+  case (src, ((dst, weight), (srcn,srccats))) =>
+    (dst, (weight, (src, srcn, srccats)))
+}).join(page_MD).flatMap({
+  case (dst, ((weight, (src, srcn, srccats)), (dstn, dstcats))) =>
+    for { sc <- srccats; dc <- dstcats } yield
+      ((sc._1, dc._1), weight * sc._2 * dc._2)
+}).reduceByKey(_+_).collect()
+
+val pw = new java.io.PrintWriter("category-edges.tsv")
+category_edges.foreach({ case ((c1,c2),w) =>
+  pw.println(c1 + "\t" + c2 + "\t" + w.toString)})
+pw.close
+
+val pw = new java.io.PrintWriter("category-weights.tsv")
+category_table.foreach({ case (c,w) => pw.println(c + "\t" + w.toString)})
+pw.close

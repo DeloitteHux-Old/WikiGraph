a4323d5994abf3528458fabd5f65fe52c425f9cd
Sam Steingold <sds@gnu.org>
2015-03-26 10:21:21 -0400

    create Ferrari subgraph

--- wikigraph.scala
+++ wikigraph.scala
@@ -6,7 +6,7 @@ import org.apache.spark.graphx._
 
 val home = "/user/misos/wikigraph/"
 
-val edges = sc.textFile(home+"edges.txt").cache()
+val edges = sc.textFile(home+"edges.txt",300).cache()
 // edges.partitions.length ==> 10
 // edges.count() ==> 4809635
 
@@ -22,10 +22,11 @@ val vertexes = sc.textFile(home+"vertex_ids.txt").map(s => { // 4831649
   val v = s.split("\t")
   (v(0),v(1).toLong)}).cache() // (page name, page ID)
 
-// use fullOuterJoin to also get vertixes without ID
+// use fullOuterJoin to also get vertexes without ID
 val page_MD = categories.rightOuterJoin(vertexes).map({ // 4,831,649
-  case (pagen,(Some(cats),id)) => (id,(pagen,cats))
-  case (pagen,(None,id)) => (id,(pagen,Array[(String,Double)]()))
+  case (pagen,(Some(cats),id)) => (id,(pagen,
+    cats.groupBy(_._1).map({ case (cat,wt) => (cat,wt(0)._2)})))
+  case (pagen,(None,id)) => (id,(pagen,Map[String,Double]()))
 }).cache()
 
 val adjacency = edges.flatMap(s => { // 116,330,611
@@ -36,10 +37,33 @@ val adjacency = edges.flatMap(s => { // 116,330,611
   })
 }).cache()
 
-val wg = Graph.apply(page_MD,adjacency).cache()
+// whole graph - don't need it!
+// val wg = Graph.apply(page_MD,adjacency).cache()
 
 // category ==> total weight (363 rows)
 val category_table = categories.flatMap(t => t._2).reduceByKey(_+_).collect()
+
+// subgraph
+val my_category = "Automotive\\Manufacturers\\Ferrari"
+val my_page_MD = page_MD.filter({ // 2867
+  case (id,(pagen,cats)) => cats.contains(my_category)
+})
+val my_adjacency = adjacency.map({case Edge(src,dst,w) =>
+  (src,(dst,w))}).join(my_page_MD).map({case (src,((dst,w),md)) =>
+    (dst,(src,w))}).join(my_page_MD).map({case (dst,((src,w),md)) =>
+      Edge(src,dst,w)}).cache() // 59733
+
+val my_graph = Graph.apply(my_page_MD,my_adjacency).cache()
+val my_pr = my_graph.staticPageRank(5).cache().vertices
+val my_indeg = my_graph.inDegrees
+val my_pages = my_page_MD.join(my_pr).join(my_indeg)
+
+val pw = new java.io.PrintWriter("ferrari.tsv")
+my_pages.foreach({ case (id,(((pagen,cats),pr),indeg)) =>
+  println(pagen + "\t" + pr.toString + "\t" + indeg.toString + "\t" +
+    cats.map({case (c,w) => c+":"+w.toString}).mkString("",",",""))})
+pw.close
+
 
 // project to categories (125784 rows)
 val category_edges = adjacency.map({
